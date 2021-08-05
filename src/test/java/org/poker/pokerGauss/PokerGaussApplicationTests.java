3846c644f5f180e41eef680e78a4be9973fba109
Benjamín Cárcamo <bicarcamo@uc.cl>
2021-08-05 17:22:37 -0400

    :star: starting test

---
+++ src/test/java/org/poker/pokerGauss/PokerGaussApplicationTests.java
@@ -0,0 +1,13 @@
+package org.poker.pokerGauss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerGaussApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
